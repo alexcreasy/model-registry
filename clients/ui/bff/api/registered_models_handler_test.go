fc135af162872033f27fe08adebc2052d868dd45
Alex Creasy <alex@creasy.dev>
2024-09-16 23:31:51 +0100

    WIP - Test refactor
    Signed-off-by: Alex Creasy <alex@creasy.dev>

--- clients/ui/bff/api/registered_models_handler_test.go
+++ clients/ui/bff/api/registered_models_handler_test.go
@@ -281,3 +281,87 @@ func TestCreateModelVersionForRegisteredModelHandler(t *testing.T) {
 	assert.Equal(t, expected.Data.Name, actual.Data.Name)
 	assert.Equal(t, rs.Header.Get("Location"), "/api/v1/model_registry/model-registry/model_versions/1")
 }
+
+func TestTest(t *testing.T) {
+	body := ModelVersionEnvelope{Data: openapi.NewModelVersion("Model One", "1")}
+
+	actual, rs, err := runTest[ModelVersionEnvelope](http.MethodPost, "/api/v1/model_registry/model-registry/registered_models/1/versions", body)
+	assert.NoError(t, err)
+
+	assert.Equal(t, http.StatusCreated, rs.StatusCode)
+
+	data := mocks.GetModelVersionMocks()[0]
+	expected := ModelVersionEnvelope{Data: &data}
+
+	assert.Equal(t, expected.Data.Name, actual.Data.Name)
+	assert.Equal(t, rs.Header.Get("Location"), "/api/v1/model_registry/model-registry/model_versions/1")
+}
+
+func Test404(t *testing.T) {
+	_, rs, err := runTest[ErrorEnvelope](http.MethodGet, "/api/v1/model_registry/model-registry/registered_models/1/versionasdf", nil)
+	assert.NoError(t, err)
+
+	assert.Equal(t, http.StatusNotFound, rs.StatusCode)
+}
+
+func runTest[T any](method string, url string, body interface{}) (T, *http.Response, error) {
+	mockMRClient, err := mocks.NewModelRegistryClient(nil)
+	if err != nil {
+		return *new(T), nil, err
+	}
+	mockK8sClient, err := mocks.NewKubernetesClient(nil)
+	if err != nil {
+		return *new(T), nil, err
+	}
+
+	mockClient := new(mocks.MockHTTPClient)
+
+	testApp := App{
+		modelRegistryClient: mockMRClient,
+		kubernetesClient:    mockK8sClient,
+	}
+
+	var req *http.Request
+	if body != nil {
+		r, err := json.Marshal(body)
+		if err != nil {
+			return *new(T), nil, err
+		}
+		bytes.NewReader(r)
+		req, err = http.NewRequest(method, url, bytes.NewReader(r))
+		if err != nil {
+			return *new(T), nil, err
+		}
+	} else {
+		req, err = http.NewRequest(method, url, nil)
+		if err != nil {
+			return *new(T), nil, err
+		}
+	}
+
+	ctx := context.WithValue(req.Context(), httpClientKey, mockClient)
+	req = req.WithContext(ctx)
+
+	rr := httptest.NewRecorder()
+
+	testApp.Routes().ServeHTTP(rr, req)
+
+	rs := rr.Result()
+	defer rs.Body.Close()
+	respBody, err := io.ReadAll(rs.Body)
+	if err != nil {
+		return *new(T), nil, err
+	}
+
+	var entity T
+	err = json.Unmarshal(respBody, &entity)
+	if err != nil {
+		if err == io.EOF {
+			// There's no body to parse.
+			return *new(T), rs, nil
+		}
+		return *new(T), nil, err
+	}
+
+	return entity, rs, nil
+}
